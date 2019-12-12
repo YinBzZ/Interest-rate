ea9da24cf2c354dfc0e89732dcdecf8098873f70
YinBzZ <49125558+YinBzZ@users.noreply.github.com>
2019-12-12 20:30:37 +0800

    实现了基本功能，修改了UI

--- js/IndexJs.js
+++ js/IndexJs.js
@@ -1,26 +1,12 @@
-// ! function(n) {
-// 	var e = n.document,
-// 		t = e.documentElement,
-// 		i = 720,
-// 		d = i / 100,
-// 		o = "orientationchange" in n ? "orientationchange" : "resize",
-// 		a = function() {
-// 			var n = t.clientWidth || 320;
-// 			n > 720 && (n = 720);
-// 			t.style.fontSize = n / d + "px"
-// 		};
-// 	e.addEventListener && (n.addEventListener(o, a, !1), e.addEventListener("DOMContentLoaded", a, !1))
-// }(window);
-
-(function() {
-	var styleN = document.createElement("style");
-	var width = document.documentElement.clientWidth / 16;
-	styleN.innerHTML = 'html{font-size:' + width + 'px!important}'
-	document.head.appendChild(styleN)
-})();
+// (function() {
+// 	var styleN = document.createElement("style");
+// 	var width = document.documentElement.clientWidth / 16;
+// 	styleN.innerHTML = 'html{font-size:' + width + 'px!important}'
+// 	document.head.appendChild(styleN)
+// })();
 
 window.onload = function() {
-	Interest_rate = 3.5;
+	Interest_rate = 3.2;
 	document.getElementById("Interest_rate").innerText = Interest_rate;
 }
 
@@ -32,11 +18,14 @@ var Interest_rate = 0,
 function Compute() {
 	StartDate = DateDeal(document.getElementById("StartDate").value);
 	var RelDate = NowDate - StartDate;
-	console.log(RelDate);
 	Money = document.getElementById("Money").value;
 	document.getElementById("Rel1").innerHTML = Math.floor(Money * Interest_rate / 100 * 100) / 100;
 	document.getElementById("Rel2").innerHTML = Math.floor(Money * Interest_rate / 100 * 1 / 360 * 100) / 100;
-	document.getElementById("Rel3").innerHTML = Math.floor(Money * Interest_rate / 100 * 1 / 360 * 100) / 100;
+	document.getElementById("Rel3").innerHTML = Math.floor(Money * Interest_rate / 100 * RelDate / 360 * 100) / 100;
+}
+
+function DateDeal(x) {
+	return x.replace(/-/g, "");
 }
 
 function getNowFormatDate() {
@@ -55,6 +44,3 @@ function getNowFormatDate() {
 	return currentdate;
 }
 
-function DateDeal(x) {
-	return x.replace(/-/g, "");
-}
---
+++ js/StyleJs.js
@@ -0,0 +1,32 @@
+var win = window;
+var doc = win.document;
+var docEl = doc.documentElement;
+var tid;
+
+function refreshRem() {
+	var width = docEl.getBoundingClientRect().width;
+	var height = docEl.getBoundingClientRect().height;
+	if ((width / height) > (750 / 1334)) {
+		var rem = height / (1334 / 50);
+	} else {
+		var rem = width / (750 / 50);
+	}
+	docEl.style.fontSize = rem + 'px';
+}
+
+win.addEventListener('resize', function() {
+	clearTimeout(tid);
+	tid = setTimeout(refreshRem, 300);
+}, false);
+win.addEventListener('pageshow', function(e) {
+	if (e.persisted) {
+		clearTimeout(tid);
+		tid = setTimeout(refreshRem, 300);
+	}
+}, false);
+
+refreshRem();
+
+window.onload = function() {
+	document.getElementById("Money").style.height = document.getElementById("Money_div").clientHeight + "px";
+}
